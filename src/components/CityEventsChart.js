5ab40c23938c6aab862e08df8646588fbc41618d
baninations <baninations@gmail.com>
2023-09-19 23:16:41 +0200

    data-visualization implementation of first chart

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -34,7 +34,7 @@ const CityEventsChart = ({ allLocations, events }) => {
           top: 20,
           right: 20,
           bottom: 60,
-          left: -30,
+          left: -10,
         }}
       >
         <CartesianGrid />
